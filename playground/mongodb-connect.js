cc1666e86317746a698ac60bd4198dc6f5050d7d
Luiz Keese Tabacow <keese.luiz@gmail.com>
2018-03-23 15:43:33 -0300

    Added connection with user authentication on MongoDB

---
+++ playground/mongodb-connect.js
@@ -0,0 +1,36 @@
+// const MongoClient = require('mongodb').MongoClient;
+//alternative of the above
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://user:password@172.17.0.3:27017/admin', (error, client) => {
+    if (error){
+        return console.log('Unable to connect to MongoDB server.\n', error);
+    }
+    console.log('Connected to MongoDB server');
+    const db = client.db('TodoApp');
+
+    
+
+    // db.collection('Todos').insertOne({
+    //     text: 'Some text',
+    //     completed: false
+    // }, (error, result) => {
+    //     if (error){
+    //         return console.log('Unable to insert todo', error);
+    //     }
+    //     console.log(JSON.stringify(result.ops, undefined, 2));
+    // });
+
+    // db.collection('Users').insertOne({
+    //     name: 'Luiz',
+    //     age: 24,
+    //     location: 'São Paulo'
+    // }, (error, result) => {
+    //     if (error){
+    //         return console.log('Unable to insert user', error);
+    //     }
+    //     console.log(JSON.stringify(result.ops, undefined, 2));
+    // });
+
+    client.close();
+});
