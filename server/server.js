4f33295989d47688bd60fba145e34db646ae3d8a
Luiz Keese Tabacow <keese.luiz@gmail.com>
2018-04-03 20:36:43 -0300

    Added route GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+const { ObjectID } = require('mongodb');
 
 var { mongoose } = require('./db/mongoose');
 var { Todo } = require('./models/todo');
@@ -25,6 +26,24 @@ app.get('/todos', (request, response) => {
     Todo.find().then((todos) => {
         response.send({todos});
     }, (error) => {
+        response.status(400).send(error);
+    });
+
+});
+
+app.get('/todos/:id', (request, response) => {
+    var id = request.params.id;
+
+    if (!ObjectID.isValid(id)){;
+        return response.status(404).send();
+    }
+
+    Todo.findById(id).then((todo) => {
+        if (!todo){
+            return response.status(404).send();
+        }
+        response.send({todo});
+    }).catch((error) => {
         response.status(400).send(error);
     });
 
